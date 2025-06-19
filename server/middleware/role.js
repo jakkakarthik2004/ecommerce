894a06e0a2903c868715750c772036e731368819
jakkakarthik04 <jsunnybabu@gmail.com>
2025-06-19 13:15:14 +0530

    changes in role.js

--- server/middleware/role.js
+++ server/middleware/role.js
@@ -4,7 +4,6 @@ const check =
     if (!req.user) {
       return res.status(401).send('Unauthorized');
     }
-
     const hasRole = roles.find(role => req.user.role === role);
     if (!hasRole) {
       return res.status(403).send('You are not allowed to make this request.');
