922d8476055ca81cf11c7ce7fd3dbc703826b76d
jakkakarthik04 <jsunnybabu@gmail.com>
2025-06-19 13:18:26 +0530

    small fix in role.js

--- server/middleware/role.js
+++ server/middleware/role.js
@@ -4,6 +4,7 @@ const check =
     if (!req.user) {
       return res.status(401).send('Unauthorized');
     }
+    
     const hasRole = roles.find(role => req.user.role === role);
     if (!hasRole) {
       return res.status(403).send('You are not allowed to make this request.');
