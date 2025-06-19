cafb8261afd05af315a6fcba2b15260533cd59f9
jakkakarthik04 <jsunnybabu@gmail.com>
2025-06-19 13:15:50 +0530

    updated apiURL

--- server/routes/index.js
+++ server/routes/index.js
@@ -5,7 +5,6 @@ const keys = require('../config/keys');
 const { apiURL } = keys.app;
 
 const api = `/${apiURL}`;
-
 // api routes
 router.use(api, apiRoutes);
 router.use(api, (req, res) => res.status(404).json('No API route found'));
